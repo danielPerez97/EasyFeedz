04465529eb54717beefcedd37c00457acd02607e
danielPerez97 <doperez1997@gmail.com>
2020-03-07 00:35:27 -0600

    Set up CSGO tweets

--- twitter/src/main/java/com/example/twitter/CSGOTweets.kt
+++ twitter/src/main/java/com/example/twitter/CSGOTweets.kt
@@ -4,6 +4,28 @@ package com.example.twitter
 
 fun csgoTweets(): List<Tweet>
 {
-    return emptyList()
+    return listOf(
+        Tweet("@brax1wnl", "I am no longer with Swole Patrol. I have decided to take a step back from competitive CS and although I may still stream CS in the future, my main goal is to await the release of Project A and put my ambitions of being the best in the world to fruition.", "https://twitter.com/brax1wnl"),
+        Tweet("@brax1wnl", "Well played by GenG. Who you guys got for the next match up between FURIA and coL? I think coL has it in the bag they’ve been looking solid imo", "https://twitter.com/brax1wnl"),
+        Tweet("@ESLCS", "Our second team for Season 11 of the #ESLProLeague is @Complexity\n" +
+                "!\n" +
+                "\n" +
+                "They have some quality players but will this season be their time to truly shine? \n" +
+                "Glowing star\n" +
+                "\n", "https://twitter.com/ESLCS"),
+        Tweet("@ESLCS", "We have the Groups for Season 11 of the #ESLProLeague!\n" +
+                "\n" +
+                "Which one are you most excited to watch? \n" +
+                "Thinking face\n", "https://twitter.com/ESLCS"),
+        Tweet("@ESLCS", "Standalone Global Elite tickets for #IEM Melbourne have SOLD OUT - although are still available (as well as limited premium tickets) as part of some new travel @KPT_AU\n" +
+                " travel packages.", "https://twitter.com/ESLCS"),
+        Tweet("@TeamLiquid", "Uniforms are still available for the class of 2020. Enroll now. ", "https://twitter.com/TeamLiquid"),
+        Tweet("@TeamLiquid", "Congratulations to @natusvincere\n" +
+                " and @s1mpleO\n" +
+                " on the all-star performance. \n" +
+                "\n" +
+                "Best of luck moving forward.", "https://twitter.com/TeamLiquid")
+        )
+
 }
 
